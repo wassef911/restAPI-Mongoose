69b9be423df30250a66fccc6553032b185f16b8b
wassef911 <1.wassef911@gmail.com>
2020-07-11 22:42:42 +0100

    init

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
 const path = require("path");
-
 const express = require("express");
+
 const app = express();
 
-const build = path.join(__dirname, "../public");
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates");
 
-app.use(express.static(build));
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Andrew Mead",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Andrew Mead",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text.",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+  });
+});
 
 app.listen(3000, () => {
   console.log("server is up on port 3000.");
