7963b40b13cf012b2ac31450bcd4db3e14a6a009
wassef911 <1.wassef911@gmail.com>
2020-08-01 22:42:32 +0100

    separated projects

--- TaskManager/mongodb.js
+++ TaskManager/mongodb.js
@@ -6,7 +6,27 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (err, client) => {
   if (err) return console.log("Unable to connect to DB .");
   console.log("connected!");
   const db = client.db(databaseName);
+  db.collection("user").updateOne({
+    _id,
+  });
+});
+// CRUD
+/*
+    CREATE 
 
+  const TASKS = [
+    { name: "task1", completed: true },
+    { name: "task2", completed: false },
+    { name: "task3", completed: true },
+  ];
+  db.collection("Tasks").insertMany(TASKS, (err, result) => {
+    if (err) return console.log(err);
+    console.log(result.ops);
+  });
+*/
+
+/*
+    READ
   db.collection("Tasks").findOne(
     { _id: ObjectID("5f1f952d2e8d5416e3bf00ea") },
     (err, user) => {
@@ -23,16 +43,4 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (err, client) => {
     });
 });
 
-/*
-        ADD 
-
-  const TASKS = [
-    { name: "task1", completed: true },
-    { name: "task2", completed: false },
-    { name: "task3", completed: true },
-  ];
-  db.collection("Tasks").insertMany(TASKS, (err, result) => {
-    if (err) return console.log(err);
-    console.log(result.ops);
-  });
 */
