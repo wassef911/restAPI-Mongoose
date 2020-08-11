6b9f0686b724b0e2410f12658ea0bb656a439a45
wassef911 <1.wassef911@gmail.com>
2020-08-11 03:33:28 +0100

    added schema timestamps

--- src/db/middleware/auth.js
+++ src/db/middleware/auth.js
@@ -13,7 +13,7 @@ const auth = async (req, res, next) => {
     if (!user) throw new Error();
     req.user = user;
     req.token = token;
-    console.log("Authentication happend.");
+    console.log("Authentication success.");
     next();
   } catch (err) {
     res.status(401).send({ error: "Please Authenticate." });
--- src/db/models/tasks.js
+++ src/db/models/tasks.js
@@ -1,14 +1,20 @@
 const mongoose = require("mongoose");
-const Tasks = mongoose.model("Tasks", {
-  description: { type: String, required: true, trim: true },
-  completed: {
-    type: Boolean,
-    default: false,
+
+const taskSchema = new mongoose.Schema(
+  {
+    description: { type: String, required: true, trim: true },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: "User",
+    },
   },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    require: true,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
+
+const Tasks = mongoose.model("Tasks", taskSchema);
 module.exports = Tasks;
