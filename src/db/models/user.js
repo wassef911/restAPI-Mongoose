40a6d22f478760635e613c0020b4f21ed68ec167
wassef911 <1.wassef911@gmail.com>
2020-08-11 03:33:43 +0100

    refactor

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -2,48 +2,53 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("../models/tasks");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true, lowercase: true },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minLength: 5,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error("Password cannot contain the word password.");
-      }
-    },
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("E-mail is invalid.");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) throw new Error("Age is a postive number.");
-    },
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true, lowercase: true },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 5,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("Password cannot contain the word password.");
+        }
       },
     },
-  ],
-});
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("E-mail is invalid.");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) throw new Error("Age is a postive number.");
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
 // methods to add method on user object
 userSchema.methods.giveAuthToken = async function () {
   const user = this;
@@ -82,8 +87,8 @@ userSchema.statics.findByCredentials = async (email, password) => {
   return user;
 };
 
+// hash the plain text password before saving
 userSchema.pre("save", async function (next) {
-  // hash the plain text password before saving
   const user = this;
   if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
@@ -91,6 +96,12 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.pre("remove", async function (next) {
+  // hash the plain text password before saving
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
+  next();
+});
 
+const User = mongoose.model("User", userSchema);
 module.exports = User;
