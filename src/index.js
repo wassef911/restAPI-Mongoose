d8181d806dfe2d45f23510f28d214c40c7532b0d
wassef911 <1.wassef911@gmail.com>
2020-08-14 06:00:23 +0100

    added app file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-const express = require("express");
+const app = require("./app");
 
-require("./db/mongoosedb");
-const userRouter = require("./routers/user");
-const tasksRouter = require("./routers/tasks");
-
-const app = express();
 const port = process.env.PORT;
-
-app.use(express.json());
-app.use(userRouter);
-app.use(tasksRouter);
 
 app.listen(port, () => {
   console.log("server is running on port " + port);
